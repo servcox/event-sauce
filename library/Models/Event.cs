a9f212c59abdb0b589a826d42680236e75439ed8
Ben Thompson <ben@thompsondigital.com.au>
2024-01-12 08:30:10 +1000

    Fixed accidental field rename in last commit

--- library/Models/Event.cs
+++ library/Models/Event.cs
@@ -3,8 +3,8 @@ namespace ServcoX.EventSauce.Models;
 public interface IEvent
 {
     String AggregateId { get; }
-    IEventPayload EventPayload { get; }
+    IEventPayload Payload { get; }
     IDictionary<String, String> Metadata { get; }
 }
 
-public readonly record struct Event(String AggregateId, IEventPayload EventPayload, IDictionary<String, String> Metadata) : IEvent;
+public readonly record struct Event(String AggregateId, IEventPayload Payload, IDictionary<String, String> Metadata) : IEvent;
