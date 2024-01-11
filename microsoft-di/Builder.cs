90d8e4e5d4b7d1c5942dcd210545f228f59b5e76
Ben Thompson <ben@thompsondigital.com.au>
2024-01-11 10:04:02 +1000

    Upgraded DI to v3

--- microsoft-di/Builder.cs
+++ microsoft-di/Builder.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ServcoX.EventSauce.Configurations;
+using ServcoX.EventSauce.V2;
+using ServcoX.EventSauce.V2.Configurations;
 
 namespace ServcoX.EventSauce;
 
 public static class Builder
 {
-    public static IServiceCollection AddEventSauce(this IServiceCollection target, String connectionString, Action<BaseConfiguration>? configure = null)
+    public static IServiceCollection AddEventSauce(this IServiceCollection target, String connectionString, String containerName, String aggregateName,
+        Action<EventStoreConfiguration>? configure = null)
     {
-        target.AddSingleton<IEventStore>(new EventStore(connectionString, configure));
+        target.AddSingleton(new EventStore(connectionString, containerName, aggregateName, configure));
+        return target;
+    }
+
+    public static IServiceCollection AddOldEventSauce(this IServiceCollection target, String connectionString, Action<BaseConfiguration>? configure = null)
+    {
+        target.AddSingleton<IEventStore>(new V2.EventStore(connectionString, configure));
         return target;
     }
 }
