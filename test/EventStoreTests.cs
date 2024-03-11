ae479cb18b495e26c22bd487b1f1b066d6d8514d
Ben Thompson <ben@thompsondigital.com.au>
2024-03-11 13:31:57 +1000

    Stubbed EventStore tests

---
+++ test/EventStoreTests.cs
@@ -0,0 +1,46 @@
+namespace ServcoX.EventSauce.Tests;
+
+public class EventStoreTests
+{
+    [Fact]
+    public async Task CanWrite()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanWriteMultiple()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanWriteNil()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanReadSince()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanAutoPollEvents()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public async Task CanManuallyPollEvents()
+    {
+        throw new NotImplementedException();
+    }\
+}
