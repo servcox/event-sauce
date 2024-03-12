a9491d79a4afe8ec2e61738777a16ba5a6c3202b
Ben Thompson <ben@thompsondigital.com.au>
2024-03-13 07:15:51 +1000

    Tidy

--- library/EventType.cs
+++ library/EventType.cs
@@ -7,33 +7,35 @@ public sealed class EventType
 {
     public String Name { get; }
 
-    public EventType (Type type)
+    public EventType(Type type)
     {
         ArgumentNullException.ThrowIfNull(type);
         Name = Encode(type);
         KnownEventBodies[Name] = type;
     }
-    
+
     public EventType(String name)
     {
         if (String.IsNullOrEmpty(name)) throw new ArgumentNullOrDefaultException(nameof(name));
         Name = name;
     }
-    
+
     public Type? TryDecode() => KnownEventBodies.GetValueOrDefault(Name);
     
+    public override String ToString() => Name;
+
     private static readonly ConcurrentDictionary<String, Type> KnownEventBodies = new();
 
     public static void Register<T>() => Register(typeof(T));
-    
+
     public static void Register(Type type)
     {
         ArgumentNullException.ThrowIfNull(type);
-        
+
         var eventType = Encode(type);
         KnownEventBodies[eventType] = type;
     }
-    
+
     private static String Encode(Type type)
     {
         if (String.IsNullOrEmpty(type.FullName)) throw new ArgumentException("Unsupported type", nameof(type));
