06cfa941e6fa5e1469aac2975c90fa327ff23adb
Ben Thompson <ben@thompsondigital.com.au>
2024-03-19 15:24:08 +1000

    Fixed broken test

--- test/EventStreamTests.cs
+++ test/EventStreamTests.cs
@@ -7,7 +7,7 @@ public sealed class EventStreamTests
 {
     private static readonly IEvent[] Decoded = [TestData.A1, TestData.A2, TestData.B];
     private static readonly String Encoded = TestData.A1Raw + TestData.A2Raw + TestData.BRaw;
-    private static readonly String EncodedIncomplete = TestData.A1Raw + TestData.A2Raw + TestData.BRaw+"abc";
+    private static readonly String EncodedIncomplete = TestData.A1Raw + TestData.A2Raw + TestData.BRaw + "abc";
 
     [Fact]
     public void CanEncode()
@@ -21,17 +21,20 @@ public sealed class EventStreamTests
     public void CanDecode()
     {
         EventType.Register<TestData.TestEventA>();
+        EventType.Register<TestData.TestEventB>();
         using var stream = new MemoryStream(Encoding.UTF8.GetBytes(Encoded));
         var (length, records) = EventStream.Decode(stream);
         length.Should().Be(Encoded.Length);
-        records.Should().BeEquivalentTo(Decoded.Select(i => new Record(TestData.At, new(i.GetType()), i)).ToList());
+        var expected = Decoded.Select(i => new Record(TestData.At, new(i.GetType()), i)).ToList();
+        records.Should().BeEquivalentTo(expected);
     }
-    
-    
+
+
     [Fact]
     public void CanDecodeIncomplete()
     {
         EventType.Register<TestData.TestEventA>();
+        EventType.Register<TestData.TestEventB>();
         using var stream = new MemoryStream(Encoding.UTF8.GetBytes(EncodedIncomplete));
         var (length, _) = EventStream.Decode(stream);
         length.Should().Be(Encoded.Length);
